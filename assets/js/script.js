6c1b174c78fa81ed9d18035e5fc24c1b2ad47617
Deku <zachdsamson@gmail.com>
2022-05-25 19:39:40 -0700

    add ability to add static task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function () {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "NEW TASK";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+buttonEl.addEventListener("click", createTaskHandler);
